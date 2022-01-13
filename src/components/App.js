195b3152669fb66ab7c127c528c3b39880a0a3af
Mary Marks <maryleemarks@gmail.com>
2022-01-13 11:44:16 -0800

    add tapList.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,8 @@ import Header from "./Header";
 import TapControl from "./TapControl";
 
 function App(){
-  const name = "Ginger";
-  const name2 = "Lemon";
+  const name = "Gingeraid";
+  const name2 = "Rose Garden";
   return (
     <React.Fragment>
       <Header />
---
+++ src/components/TapList.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Tap from "./Tap";
+import PropTypes from "prop-types";
+
+function TapList(props){
+  return (
+    <React.Fragment>
+      <hr/>
+      {props.tapList.map((event) =>
+        <Event
+          whenTapClicked = { props.onTapSelection }
+          name={tap.name}
+          brand={tap.brand}
+          price={tap.price}
+          flavor={tap.flavor}
+          id={tap.id}
+          key={tap.id}/>
+        )}
+    </React.Fragment>
+  );
+}
+
+TapList.propTypes = {
+  tapList: PropTypes.array,
+  onTapSelection: PropTypes.func
+};
+
+export default TapList;
