1bc1344f3923d7b46863ad88bc71fcf22a4cdac5
Mary Marks <maryleemarks@gmail.com>
2022-01-14 13:24:18 -0800

    add image to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from "react";
+import kombucha from './../img/kombucha.jpg';
 
 function Header(){
   return (
     <React.Fragment>
       <h1>Welcome to the Kombucha Tap Room</h1>
+      <img src={kombucha} alt="an image of kombucha taps"/>
     </React.Fragment>
   );
 }
--- src/components/ReusableForm.js
+++ src/components/ReusableForm.js
@@ -19,6 +19,7 @@ function ReusableForm(props) {
           placeholder='Flavor' />
         <input
           type = 'number'
+          step = '0.01'
           name = 'price'
           placeholder = 'price'/>  
         <button type='submit'>{props.buttonText}</button>
